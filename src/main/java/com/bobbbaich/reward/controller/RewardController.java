79c748ddb6aa09324992e5fd189ab9fc50682e50
bohdanpohorilyi <Bpogori1>
2022-06-15 22:06:06 +0300

    update health check

--- src/main/java/com/bobbbaich/reward/controller/RewardController.java
+++ src/main/java/com/bobbbaich/reward/controller/RewardController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/reward")
+@RequestMapping("/api/rewards/gifts")
 public class RewardController {
 
     @GetMapping
