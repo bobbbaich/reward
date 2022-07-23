56c0795347929341f7bbf8232a3ad3939bdce85b
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2022-07-23 20:21:27 +0300

    update: add aspects

--- src/main/java/com/bobbbaich/reward/configuration/XRayConfiguration.java
+++ src/main/java/com/bobbbaich/reward/configuration/XRayConfiguration.java
@@ -1,14 +1,18 @@
 package com.bobbbaich.reward.configuration;
 
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.spring.aop.AbstractXRayInterceptor;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.cloud.sleuth.brave.LocalServiceName;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
 
+@Aspect
 @Configuration
-public class XRayConfiguration {
+public class XRayConfiguration extends AbstractXRayInterceptor {
 
     @LocalServiceName
     private String serviceName;
@@ -16,5 +20,10 @@ public class XRayConfiguration {
     @Bean
     public Filter TracingFilter() {
         return new AWSXRayServletFilter(serviceName);
+    }
+
+    @Override
+    @Pointcut("@within(com.amazonaws.xray.spring.aop.XRayEnabled) && within(com.bobbbaich.reward.*))")
+    protected void xrayEnabledClasses() {
     }
 }
