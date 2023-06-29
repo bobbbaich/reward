7e09e5c8e82cf27cf874bef06c4a9bb861491c80
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2023-06-29 18:49:29 +0300

    merge deployments

--- src/main/java/com/bobbbaich/reward/configuration/EventBridgeConfiguration.java
+++ src/main/java/com/bobbbaich/reward/configuration/EventBridgeConfiguration.java
@@ -1,11 +1,8 @@
 package com.bobbbaich.reward.configuration;
 
-import io.awspring.cloud.autoconfigure.core.RegionProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.services.eventbridge.EventBridgeAsyncClient;
 
 @Slf4j
@@ -13,12 +10,7 @@ import software.amazon.awssdk.services.eventbridge.EventBridgeAsyncClient;
 public class EventBridgeConfiguration {
 
     @Bean
-    public EventBridgeAsyncClient eventBridgeClient(@Value("${aws.region}") String reg,
-                                                    AwsCredentialsProvider awsCredentialsProvider,
-                                                    RegionProperties regionProperties) {
-        log.info("reg=" + reg);
-        log.info("awsCredentialsProvider=", awsCredentialsProvider);
-        log.info("regionProperties=={}", regionProperties);
+    public EventBridgeAsyncClient eventBridgeClient() {
         return EventBridgeAsyncClient.builder()
                 .build();
     }
