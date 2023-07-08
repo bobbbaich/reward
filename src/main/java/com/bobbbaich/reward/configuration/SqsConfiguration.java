9e3bf34a11d95ba8ccc7fdff053f6694160b88b9
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2023-07-08 16:12:16 +0300

    fix tests

--- src/main/java/com/bobbbaich/reward/configuration/SqsConfiguration.java
+++ src/main/java/com/bobbbaich/reward/configuration/SqsConfiguration.java
@@ -1,7 +1,6 @@
 package com.bobbbaich.reward.configuration;
 
 import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
-import io.awspring.cloud.sqs.listener.QueueNotFoundStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
@@ -11,26 +10,12 @@ import static io.awspring.cloud.sqs.listener.QueueNotFoundStrategy.FAIL;
 @Configuration
 public class SqsConfiguration {
 
-    private static final QueueNotFoundStrategy QUEUE_NOT_FOUND_STRATEGY = FAIL;
-
-//    @Bean
-//    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient,
-//                                   ObjectProvider<ObjectMapper> objectMapperProvider) {
-//        SqsTemplateBuilder builder = SqsTemplate.builder();
-//        objectMapperProvider
-//                .ifAvailable(om -> builder.configureDefaultConverter(converter -> converter.setObjectMapper(om)));
-//        return builder.sqsAsyncClient(sqsAsyncClient)
-//                .configure(options -> options
-//                        .queueNotFoundStrategy(QUEUE_NOT_FOUND_STRATEGY))
-//                .build();
-//    }
-
     @Bean
-    public SqsMessageListenerContainerFactory<Object> sqsMessageListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
+    public SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
         return SqsMessageListenerContainerFactory
                 .builder()
                 .configure(options -> options
-                        .queueNotFoundStrategy(QUEUE_NOT_FOUND_STRATEGY)
+                        .queueNotFoundStrategy(FAIL)
                 )
                 .sqsAsyncClient(sqsAsyncClient)
                 .build();
--- src/test/java/com/bobbbaich/reward/configuration/LocalstackConfiguration.java
+++ src/test/java/com/bobbbaich/reward/configuration/LocalstackConfiguration.java
@@ -1,12 +1,18 @@
 package com.bobbbaich.reward.configuration;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.SQS;
 
+@Slf4j
 @Configuration
 public class LocalstackConfiguration {
 
@@ -21,5 +27,22 @@ public class LocalstackConfiguration {
                 .withServices(SQS)
                 .withReuse(true);
         LOCALSTACK.start();
+    }
+
+    @Value("${service.sqs.gift-lifecycle}")
+    private String giftLifecycleQueue;
+
+    @Autowired
+    private SqsAsyncClient sqsAsyncClient;
+
+    @PostConstruct
+    private void setupAws() {
+        createQueue(giftLifecycleQueue);
+    }
+
+    private void createQueue(String queueName) {
+        sqsAsyncClient
+                .createQueue(builder -> builder.queueName(queueName))
+                .thenRun(() -> log.info("queue created queueName={}", queueName));
     }
 }
