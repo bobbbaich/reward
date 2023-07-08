2a414d8450193f1f12f9e76c7e53a1ff80625100
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2023-07-08 15:21:35 +0300

    configure sqs container

--- src/main/java/com/bobbbaich/reward/configuration/SqsConfiguration.java
+++ src/main/java/com/bobbbaich/reward/configuration/SqsConfiguration.java
@@ -1,26 +1,38 @@
 package com.bobbbaich.reward.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
 import io.awspring.cloud.sqs.listener.QueueNotFoundStrategy;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
-import io.awspring.cloud.sqs.operations.SqsTemplateBuilder;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
+import static io.awspring.cloud.sqs.listener.QueueNotFoundStrategy.FAIL;
+
 @Configuration
 public class SqsConfiguration {
 
+    private static final QueueNotFoundStrategy QUEUE_NOT_FOUND_STRATEGY = FAIL;
+
+//    @Bean
+//    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient,
+//                                   ObjectProvider<ObjectMapper> objectMapperProvider) {
+//        SqsTemplateBuilder builder = SqsTemplate.builder();
+//        objectMapperProvider
+//                .ifAvailable(om -> builder.configureDefaultConverter(converter -> converter.setObjectMapper(om)));
+//        return builder.sqsAsyncClient(sqsAsyncClient)
+//                .configure(options -> options
+//                        .queueNotFoundStrategy(QUEUE_NOT_FOUND_STRATEGY))
+//                .build();
+//    }
+
     @Bean
-    public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient,
-                                   ObjectProvider<ObjectMapper> objectMapperProvider) {
-        SqsTemplateBuilder builder = SqsTemplate.builder();
-        objectMapperProvider
-                .ifAvailable(om -> builder.configureDefaultConverter(converter -> converter.setObjectMapper(om)));
-        return builder.sqsAsyncClient(sqsAsyncClient)
+    public SqsMessageListenerContainerFactory<Object> sqsMessageListenerContainerFactory(SqsAsyncClient sqsAsyncClient) {
+        return SqsMessageListenerContainerFactory
+                .builder()
                 .configure(options -> options
-                        .queueNotFoundStrategy(QueueNotFoundStrategy.FAIL))
+                        .queueNotFoundStrategy(QUEUE_NOT_FOUND_STRATEGY)
+                )
+                .sqsAsyncClient(sqsAsyncClient)
                 .build();
     }
 }
